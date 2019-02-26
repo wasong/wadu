1dffcb0d79d59e4e93dd4a9e3166a550e28836f1
Andrew Song <andrewgnos@gmail.com>
2019-02-26 15:26:10 -0800

    add connector

--- tools/webpack.common.js
+++ tools/webpack.common.js
@@ -26,9 +26,9 @@ const config = {
   resolve: {
     alias: {
       components: path.resolve(__dirname, '../src/components/'),
-      theme: path.resolve(__dirname, '../src/styles/theme.js'),
       utils: path.resolve(__dirname, '../src/utils/'),
       assets: path.resolve(__dirname, '../public/assets/'),
+      theme: path.resolve(__dirname, '../src/styles/theme.js'),
       styler: path.resolve(__dirname, '../src/styles/styler.js'),
       compose: path.resolve(__dirname, '../src/styles/compose.js'),
     },
@@ -44,7 +44,7 @@ const config = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        VERSION: JSON.stringify(process.env.VERSION || 'omnae'),
+        VERSION: JSON.stringify(process.env.VERSION || 'wadu'),
         API_URL: JSON.stringify(process.env.API_URL || 'http://localhost:9090'),
         AUTH0_CLIENT_ID: JSON.stringify(process.env.AUTH0_CLIENT_ID || 'dUeE42A2aB9grdcVjVP5DD47aEHxYZ7R'),
         AUTH0_DOMAIN: JSON.stringify(process.env.AUTH0_DOMAIN || 'INSERT_DOMAIN_HERE.auth0.com'),
