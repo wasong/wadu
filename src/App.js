b9b3cea1f7b69c4518d95e75bcaf0d596722b6bc
Andrew Song <andrewgnos@gmail.com>
2019-10-05 18:17:46 -0700

    remove mui

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import { Provider } from 'react-redux'
 import { hot } from 'react-hot-loader/root'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
 import store from 'utils/store'
-import muiTheme from './styles/muiTheme'
 import { authenticate } from './app.module'
 
 import Routes from './routes'
@@ -17,28 +14,25 @@ class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider theme={muiTheme}>
-        <Provider store={store}>
-          <div>
-            <CssBaseline />
-            <Helmet
-              titleTemplate="%s | Wadu"
-              meta={[
-                { charset: 'utf-8' },
-                {
-                  'http-equiv': 'X-UA-Compatible',
-                  content: 'IE=edge',
-                },
-                {
-                  name: 'viewport',
-                  content: 'width=device-width, initial-scale=1',
-                },
-              ]}
-            />
-            <Routes />
-          </div>
-        </Provider>
-      </MuiThemeProvider>
+      <Provider store={store}>
+        <div>
+          <Helmet
+            titleTemplate="%s | Wadu"
+            meta={[
+              { charset: 'utf-8' },
+              {
+                'http-equiv': 'X-UA-Compatible',
+                content: 'IE=edge',
+              },
+              {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1',
+              },
+            ]}
+          />
+          <Routes />
+        </div>
+      </Provider>
     )
   }
 }
